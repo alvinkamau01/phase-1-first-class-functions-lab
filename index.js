a98250013697da67d0a94b78a362978018e128cf
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-07-04 00:41:05 +0300

    Assignments

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+function returnLastTwoDrivers(drivers){
+   return drivers.slice(-2)
+  }
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(argument){
+    return function fareMultiplier(base){
+       return argument*base
+    }
+}
+
+function fareDoubler(fare){
+   return fare*2
+}
+
+function fareTripler(fare){
+    return fare*3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, Function) {
+    return Function(arrayOfDrivers)
+  } 
+  console.log(selectDifferentDrivers)
